1964682d271e18e45c62e09306023424714b712b
Theekshana <47950733+wasthTheekshana@users.noreply.github.com>
2021-11-08 22:16:36 +0530

    update match page

--- IPL Dashboard/IPL-Dashboard/src/frontend/src/pages/MatchPage.js
+++ IPL Dashboard/IPL-Dashboard/src/frontend/src/pages/MatchPage.js
@@ -1,15 +1,32 @@
 import {React, useEffect, useState} from 'react';
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {MatchDetailCard} from "../components/MatchDetailCard";
 import {MathSmallCard} from "../components/MathSmallCard";
 
 export const MatchPage = () => {
 
-    return (
-        <div className="MatchPage">
-            <h1>MatchPage</h1>
+    const [matches, setMatches] = useState([]);
 
-        </div>
+    const { teamName,year } = useParams();
+    useEffect(
+        () =>{
+            const fetchMatches = async () => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const data = await response.json();
+                console.log(data);
+                setMatches(data);
+            };
+            fetchMatches();
+        },[]
     );
+
+return (
+    <div className="MatchPage">
+        <h1>MatchPage</h1>
+        {
+            matches.map(match => <MatchDetailCard teamName={teamName} match={match}/>)
+        }
+    </div>
+);
 }
 
